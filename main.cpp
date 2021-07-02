ea864a96d4152e9e4d69781138d1fd4a66159f3c
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2021-07-02 23:24:27 +1000

    Add upscaling

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <raylib.h>
 
-void update() {
+void Update() {
 
 }
 
-void draw() {
-    BeginDrawing();
-
+void Draw() {
     ClearBackground(RAYWHITE);
 
-    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-    EndDrawing();
+    DrawText("Congrats! You created your first window!", 25, 80, 5, LIGHTGRAY);
 }
 
 int main() {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    int windowWidth = 1024;
+    int windowHeight = 768;
 
-    InitWindow(screenWidth, screenHeight, "malloc(memory);");
+    //SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(windowWidth, windowHeight, "malloc(memory);");
+
+    int renderWidth = 256;
+    int renderHeight = 192;
+    RenderTexture2D screenTexture = LoadRenderTexture(renderWidth, renderHeight);
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -30,12 +31,35 @@ int main() {
     {
         // Update
         //----------------------------------------------------------------------------------
-        update();
+        Update();
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
-        draw();
+
+        BeginTextureMode(screenTexture);
+            Draw();
+        EndTextureMode();
+
+        //windowWidth = GetScreenWidth();
+        //windowHeight = GetScreenHeight();
+
+        BeginDrawing();
+            Rectangle textureRec;
+            textureRec.x = 0;
+            textureRec.y = 0;
+            textureRec.width = (float)screenTexture.texture.width;
+            textureRec.height = (float)-screenTexture.texture.height;
+            Rectangle windowRec;
+            windowRec.x = 0;
+            windowRec.y = 0;
+            windowRec.width = (float)windowWidth;
+            windowRec.height = (float)windowHeight;
+            Vector2 position;
+            position.x = 0;
+            position.y = 0;
+            DrawTexturePro(screenTexture.texture, textureRec, windowRec, position, 0, WHITE);
+        EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
